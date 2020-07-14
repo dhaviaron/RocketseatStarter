9e903b0252310daf6f63987eedfaaa567bcc7fd4
DAWestphal <56050283+DAWestphal@users.noreply.github.com>
2020-07-14 14:40:58 -0300

    conectando com banco de dados

--- node/server.js
+++ node/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 const app = express()
 
+mongoose.connect('mongodb://localhost:27017/nodeapi',{useUnifiedTopology: true,  useNewUrlParser: true})
 app.get('/',(req, res) =>{
     res.send("Hey Fellas!")
 })
