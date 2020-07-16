703144cf568e9764f2827fa6c8d10c3565773608
DAWestphal <56050283+DAWestphal@users.noreply.github.com>
2020-07-16 17:34:41 -0300

    listando produtos

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,8 +1,35 @@
 import React, {Component} from 'react'
 import api from '../../services/api.js'
-
+import './styles.css'
 export default class Main extends Component{
+    state = {
+        products: []
+    }
+    componentDidMount(){
+        this.loadProducts()
+    }
+    loadProducts = async () => {
+        const response = await api.get('products')
+        this.setState({ products: response.data.docs})
+    }
+
     render() {
-        return <h1>Hello Rocketseat</h1>
+        const{products} = this.state
+        return (
+            <div className= 'product-list'>
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                    
+                    <a href="">Acessar</a>
+                    </article>
+                ))}
+                <div className="actions">
+                    <button>Anterior</button>
+                    <button>Próximo</button>
+                </div>
+            </div>
+        )
     }
 }
